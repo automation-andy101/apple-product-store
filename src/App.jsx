7fe77ec1c5316ca6454b721157307688605a627e
automation.andy101 <automation.andy101@gmail.com>
2025-10-09 14:29:44 +0100

    Added IPhone, Watch and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react'
 import Controls from './components/Controls'
+import Navbar from './components/NavBar'
+import Home from './components/Home'
+import IPhone from './components/IPhone'
+import MacBook from './components/MacBook'
+import Watch from './components/Watch'
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
+  const [activePage, setActivePage] = useState(0)
+
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex)
+  }
 
   const toggleZoom = () => {
     setFrameZoom(!frameZoom)
@@ -10,8 +20,15 @@ const App = () => {
 
   return (
     <div className='w-full h-screen grid place-items-center'>
-      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
+      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 flex`}>
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
+        <div className="flex-grow">
+          {/* <Home /> */}
+          {/* <IPhone /> */}
+          {/* <MacBook /> */}
+          <Watch />
+        </div>
       </div>
     </div>
   )
