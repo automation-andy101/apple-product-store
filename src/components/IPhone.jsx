6b78d737abe9b7435d4cfd155ae4f79973c9925b
automation.andy101 <automation.andy101@gmail.com>
2025-10-21 21:21:09 +0100

    Added show/gide navbar functionality

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -9,7 +9,7 @@ const IPhone = () => {
         </h3>
         <div className="flex justify-between w-full">
             {iphoneModels.map((iphone, index) => (
-                <div key={index} className='flex flex-col items-center justify-between gap-y-2'>
+                <div key={index} className={`flex flex-col items-center justify-between gap-y-2 ${index === 2 ? 'hidden md:flex' : ''}`}>
                     <img 
                         src={iphone.img} 
                         alt={iphone.name} 
